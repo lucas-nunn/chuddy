d07e254b6e8606be554c34ebe3de1bc8f653cda6
Lucas <lucas@lucasnunn.me>
2022-11-24 16:58:54 -0500

    added some animations when scrolling

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-/* scroll to projects upon scroll click */
+/* scroll button to scroll up and down */
 let atTop = true;
-const scroll= document.getElementById('scroll');
-const frontPage = document.getElementById("front-page");
+const scroll= document.getElementById("scroll");
+const intro = document.getElementById("intro");
 const projects = document.getElementById("projects");
+const git = document.getElementById("git");
+const linked = document.getElementById("linked");
 
 scroll.onclick = () => {
+  /* animate the navbar items */
+  scroll.classList.toggle("flip");
+  git.classList.toggle("trans-left");
+  linked.classList.toggle("trans-right");
+
   if (atTop) {
-    projects.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
     atTop = false;
+    projects.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
   else {
-    frontPage.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
     atTop = true;
+    intro.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 }
