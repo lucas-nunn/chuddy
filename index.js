0d98660442aedc4e56af945b1d16113bafb2b316
Lucas <lucas@lucasnunn.me>
2022-11-26 10:28:40 -0500

    improved navbar items animation

--- index.js
+++ index.js
@@ -5,51 +5,41 @@ contactOpen.onclick = () => {
   contact.style.display = "flex"; 
 }
 
-/* scroll-relevant elements */
+/* ----- animate navbar items when scrolling ----- */
 const scroll= document.getElementById("scroll");
 const git = document.getElementById("git");
 const linked = document.getElementById("linked");
+
+const checkScrollDistance = () => {
+  const scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  const half = document.documentElement.clientHeight * 0.75;
+
+  if (scrollTop < half && scroll.classList.contains("flip")) {
+    scroll.classList.remove("flip"); 
+    git.classList.remove("trans-left");
+    linked.classList.remove("trans-right");
+  }
+  else if (scrollTop > half && !scroll.classList.contains("flip")) {
+    scroll.classList.add("flip"); 
+    git.classList.add("trans-left");
+    linked.classList.add("trans-right");
+  }
+  
+}
+
+document.addEventListener("scroll", checkScrollDistance);
+
+/* ----- scroll up / down when clicking scroll button ----- */
 const intro = document.getElementById("intro");
 const projects = document.getElementById("projects");
-let scrollThreshReached = false;
-let scrollFinished = null;
 
-/* scroll up/down and animate navbar items */
-const toggleScroll = () => {
-  // remove scroll event listener until scroll finishes
-  document.removeEventListener("scroll", checkToggle); 
-  if (scrollFinished === null) {
-    clearTimeout(scrollFinished);
-  }  
-
-  scrollFinished = setTimeout(() => document.addEventListener("scroll", checkToggle), 500);
-
-  // scroll up or down
-  if (scrollThreshReached) {
-   intro.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+scroll.addEventListener("click", () => {
+  if (scroll.classList.contains("flip")) {
+    intro.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
   else {
     projects.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
-  scrollThreshReached = !scrollThreshReached;
 
-  // animate navbar items
-  scroll.classList.toggle("flip");
-  git.classList.toggle("trans-left");
-  linked.classList.toggle("trans-right");
-}
+});
 
-/* scroll up/down if scroll threshold crossed */
-const checkToggle = () => {
-  const scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-  const half = document.documentElement.clientHeight / 2;
-
-  if ((scrollThreshReached && scrollTop < half) ||
-      !scrollThreshReached && scrollTop > half) {
-    toggleScroll();
-  }
-}
-
-/* scroll up/down on clicking scroll button or crossing scroll threshold */
-document.addEventListener("scroll", checkToggle);
-scroll.addEventListener("click", toggleScroll);
