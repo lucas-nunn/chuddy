5006634604ff6c467fd5822afd9845db98f9d99a
Lucas <lucas@lucasnunn.me>
2022-12-01 14:02:52 -0500

    ui updates and code formatting

--- js/contact.js
+++ js/contact.js
@@ -1,44 +1,51 @@
 /* ----- open, close, and submit contact form ----- */
-const main = document.getElementById("main")
-const contact = document.getElementById("contact")
-const contactOpen = document.getElementById("contact-open")
+const main = document.getElementById("main");
+const contact = document.getElementById("contact");
+const contactOpen = document.getElementById("contact-open");
+const sendMessage = document.getElementById("send-message");
 
 /* check for background clicks to close contact form */
 const closeViaBackground = (event) => {
   if (event.srcElement.id === "contact") {
-    document.removeEventListener("click", closeViaBackground)
+    document.removeEventListener("click", closeViaBackground);
 
-    contact.style.display = "none"
-    main.style.overflow = "visible"
-    send.style.display = "none"
+    contact.style.display = "none";
+    main.style.overflow = "visible";
+    send.style.display = "none";
 
-    email.value = ""
-    message.value = ""
+    email.value = "";
+    message.value = "";
   }
-}
+};
 
+/* open contact form upon button click */
 contactOpen.onclick = () => {
-  contact.style.display = "flex"
-  main.style.overflow = "hidden"
-  
-  document.addEventListener("click", closeViaBackground)
-}
+  contact.style.display = "flex";
+  main.style.overflow = "hidden";
 
+  document.addEventListener("click", closeViaBackground);
+};
+
+/* send message upon button click */
+sendMessage.onclick = (event) => {
+  // emailjs API data
+  const data = {
+    service_id: "",
+  };
+};
 
 /* ----- validate input -----  */
-const email = document.getElementById("email")
-const message = document.getElementById("message")
-const send = document.getElementById("send-message")
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+const send = document.getElementById("send-message");
 
 const validateInput = () => {
   if (email.value.length > 5 && message.value.length > 20) {
-    send.style.display = "block"
+    send.style.display = "block";
+  } else {
+    send.style.display = "none";
   }
-  else {
-    send.style.display = "none"
-  }
-}
+};
 
-email.addEventListener("input", validateInput)
-message.addEventListener("input", validateInput)
-
+email.addEventListener("input", validateInput);
+message.addEventListener("input", validateInput);
--- js/scroll.js
+++ js/scroll.js
@@ -1,38 +1,54 @@
-/* ----- animate navbar items when scrolling ----- */
-const scroll= document.getElementById("scroll");
+/**
+ * animate navbar items when scrolling
+ */
+
+/* relevant html elements */
+const scroll = document.getElementById("scroll");
 const git = document.getElementById("git");
 const linked = document.getElementById("linked");
 
+/*  */
 const checkScrollDistance = () => {
-  const scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  const scrollTop =
+    window.pageYOffset ||
+    (document.documentElement || document.body.parentNode || document.body)
+      .scrollTop;
   const half = document.documentElement.clientHeight * 0.75;
 
   if (scrollTop < half && scroll.classList.contains("flip")) {
-    scroll.classList.remove("flip"); 
+    scroll.classList.remove("flip");
     git.classList.remove("trans-left");
     linked.classList.remove("trans-right");
-  }
-  else if (scrollTop > half && !scroll.classList.contains("flip")) {
-    scroll.classList.add("flip"); 
+  } else if (scrollTop > half && !scroll.classList.contains("flip")) {
+    scroll.classList.add("flip");
     git.classList.add("trans-left");
     linked.classList.add("trans-right");
   }
-  
-}
+};
 
 document.addEventListener("scroll", checkScrollDistance);
 
-/* ----- scroll up / down when clicking scroll button ----- */
+/**
+ * scroll up / down when clicking scroll button
+ */
+
+/* relevant html elements */
 const intro = document.getElementById("intro");
 const projects = document.getElementById("projects");
 
+/* */
 scroll.addEventListener("click", () => {
   if (scroll.classList.contains("flip")) {
-    intro.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    intro.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  } else {
+    projects.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
   }
-  else {
-    projects.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-  }
-
 });
-
