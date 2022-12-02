8b167e7423b22174f5bb6cead57fd78290f89740
Lucas <lucas@lucasnunn.me>
2022-12-02 01:28:53 -0500

    contact form improvements

--- js/contact.js
+++ js/contact.js
@@ -1,49 +1,90 @@
-/* ----- open, close, and submit contact form ----- */
-const main = document.getElementById("main");
-const contact = document.getElementById("contact");
-const contactOpen = document.getElementById("contact-open");
+/**
+ * validate form input
+ */
+
+/* relevant html elements */
+const emailInput = document.getElementById("email-input");
+const messageInput = document.getElementById("message-input");
 const sendMessage = document.getElementById("send-message");
+
+/* ensure each input has enough characters */
+const validateInput = () => {
+  if (emailInput.value.length > 5 && messageInput.value.length > 20) {
+    sendMessage.style.display = "block";
+  } else {
+    sendMessage.style.display = "none";
+  }
+};
+
+emailInput.addEventListener("input", validateInput);
+messageInput.addEventListener("input", validateInput);
+
+/**
+ * open, close, and submit contact form
+ */
+const main = document.getElementById("main");
+const contactPopup = document.getElementById("contact-popup");
+const contactOpen = document.getElementById("contact-open");
+const contactForm = document.getElementById("contact-form");
+const loader = document.getElementById("loader");
+const successMessage = document.getElementById("success-message");
+const failureMessage = document.getElementById("failure-message");
+let timeout = null;
+
+/* close the form and reset its fields */
+const closeForm = () => {
+  contactPopup.style.display = "none";
+  contactForm.style.display = "flex";
+  sendMessage.style.display = "none";
+  loader.style.display = "none";
+  successMessage.style.display = "none";
+  failureMessage.style.display = "none";
+  main.style.overflow = "visible";
+
+  emailInput.value = "";
+  messageInput.value = "";
+};
 
 /* check for background clicks to close contact form */
 const closeViaBackground = (event) => {
-  if (event.srcElement.id === "contact") {
+  if (event.srcElement.id === "contact-popup") {
     document.removeEventListener("click", closeViaBackground);
 
-    contact.style.display = "none";
-    main.style.overflow = "visible";
-    send.style.display = "none";
+    if (timeout) {
+      clearTimeout(timeout);
+    }
 
-    email.value = "";
-    message.value = "";
+    closeForm();
   }
 };
 
 /* open contact form upon button click */
 contactOpen.onclick = () => {
-  contact.style.display = "flex";
+  contactPopup.style.display = "flex";
   main.style.overflow = "hidden";
 
   document.addEventListener("click", closeViaBackground);
 };
 
 /* send message upon button click */
-sendMessage.onclick = (event) => {
-  // emailjs API data
-  const URL = "https://api.emailjs.com/api/v1.0/email/send";
-
+sendMessage.onclick = () => {
   // if you are reading this I know you can steal and use my API keys
   // if you do then you are a bum
+  const URL = "https://api.emailjs.com/api/v1.0/email/send";
+
   const data = {
     service_id: "default_service",
     template_id: "template_i71q91o",
     user_id: "T-p91i_grYBseQg2d",
     template_params: {
-      email: email.value,
-      message: message.value,
+      email: emailInput.value,
+      message: messageInput.value,
     },
   };
 
-  // hit the emailjs API
+  contactForm.style.display = "none";
+  loader.style.display = "block";
+
   fetch(URL, {
     method: "POST",
     headers: {
@@ -52,22 +93,16 @@ sendMessage.onclick = (event) => {
     },
     body: JSON.stringify(data),
   })
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
+    .then((response) => {
+      console.log(response);
+      successMessage.style.display = "block";
+      timeout = setTimeout(() => closeForm(), 5000);
+    })
+    .catch((error) => {
+      console.log(error);
+      failureMessage.style.display = "block";
+    })
+    .finally(() => {
+      loader.style.display = "none";
+    });
 };
-
-/* ----- validate input -----  */
-const email = document.getElementById("email");
-const message = document.getElementById("message");
-const send = document.getElementById("send-message");
-
-const validateInput = () => {
-  if (email.value.length > 5 && message.value.length > 20) {
-    send.style.display = "block";
-  } else {
-    send.style.display = "none";
-  }
-};
-
-email.addEventListener("input", validateInput);
-message.addEventListener("input", validateInput);
